e88461c8a20b2887b433206f609868a5de4bedcc
Martin Herrmann <martin@famherrmann.de>
2022-10-09 23:23:20 +0200

    Add iterable.grouper

---
+++ src/elastic_tabs/iterable.py
@@ -0,0 +1,8 @@
+from typing import Iterable
+
+# TODO test
+def grouper(n: int, iterable: Iterable, fill_value=None) -> Iterable:
+    """From https://docs.python.org/3/library/itertools.html#itertools-recipes"""
+    from itertools import zip_longest
+    args = [iter(iterable)] * n
+    return zip_longest(fillvalue=fill_value, *args)
