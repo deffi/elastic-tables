53021756d67a8f15c360d47515c333f17a206adb
Martin Herrmann <martin@famherrmann.de>
2022-10-11 22:37:53 +0200

    Add TableGenerator

--- src/elastic_tabs/parsing/__init__.py
+++ src/elastic_tabs/parsing/__init__.py
@@ -1,2 +1,3 @@
 from .line_splitter import LineSplitter
 from .block_splitter import BlockSplitter
+from .table_generator import TableGenerator
---
+++ tests/parsing/test_table_generator.py
@@ -0,0 +1,33 @@
+import unittest
+
+from elastic_tabs.model import Table, Row, Block, Line
+from elastic_tabs.parsing import TableGenerator
+
+
+class TableGeneratorTest(unittest.TestCase):
+    def test_empty(self):
+        self.assertEqual(Table([]), TableGenerator().table_from_block(Block([])))
+
+    def test_blank(self):
+        self.assertEqual(Table([Row([""], "")]), TableGenerator().table_from_block(Block([
+            Line("", ""),
+        ])))
+
+    def test_block(self):
+        block = Block([
+            Line("foo\tb", "\n"),
+            Line("or", "\n"),
+            Line("f\tbar", "\n"),
+        ])
+
+        expected = Table([
+            Row(["foo", "b"], "\n"),
+            Row(["or"], "\n"),
+            Row(["f", "bar"], "\n"),
+        ])
+
+        self.assertEqual(expected, TableGenerator().table_from_block(block))
+
+
+if __name__ == '__main__':
+    unittest.main()
