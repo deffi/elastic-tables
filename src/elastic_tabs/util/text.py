c439f6be87cdde17a92ca1996fe1f4f8f70bab9b
Martin Herrmann <martin@famherrmann.de>
2022-10-17 22:54:02 +0200

    Remove old function

--- src/elastic_tabs/util/text.py
+++ src/elastic_tabs/util/text.py
@@ -1,37 +1,8 @@
 import re
-from typing import Tuple, Sequence, Iterator
+from typing import Tuple, Iterator
 
 from elastic_tabs.model import Line
 from elastic_tabs.util.iterable import grouper
-
-
-# TODO remove
-def splitlines(string: str, keep_ends: bool = False) -> Tuple[Sequence[str], str]:
-    """Returns lines, remainder"""
-
-    # str.splitlines will include unterminated lines, unless they are empty:
-    #     ""           -> []
-    #     "\n"         -> [""]
-    #     "foo\nbar"   -> ["foo", "bar"]
-    #     "foo\nbar\n" -> ["foo", "bar"]
-    #
-    # We want to return the unterminated part as remainder, regardless of
-    # whether it is empty. So we terminate it by appending an newline and use
-    # the last line as remainder. This also ensures that there will always be at
-    # least one line in the result from splitlines, even if string is empty.
-
-    # Append a newline and split
-    lines = (string + "\n").splitlines(keepends=keep_ends)
-
-    # The last line is the remainder, the others are the result
-    lines, remainder = lines[:-1], lines[-1]
-
-    # If keep_ends is true, then the remainder will include the newline we
-    # appended
-    if keep_ends:
-        remainder = remainder[:-1]
-
-    return lines, remainder
 
 
 def split_lines(string: str) -> Tuple[Iterator[Line], str]:
--- tests/util/test_text.py
+++ tests/util/test_text.py
@@ -1,24 +1,10 @@
 import unittest
 
 from elastic_tabs.model import Line
-from elastic_tabs.util.text import splitlines, split_lines
+from elastic_tabs.util.text import split_lines
 
 
 class TextTest(unittest.TestCase):
-    def test_keep_ends_false(self):
-        self.assertEqual(([], ""), splitlines(""))
-        self.assertEqual(([""], ""), splitlines("\n"))
-        self.assertEqual((["foo", "bar"], ""), splitlines("foo\nbar\n"))
-        self.assertEqual((["foo"], "bar"), splitlines("foo\nbar"))
-        self.assertEqual((["foo", "", "bar"], ""), splitlines("foo\n\nbar\n"))
-
-    def test_keep_ends_true(self):
-        self.assertEqual(([], ""), splitlines("", keep_ends=True))
-        self.assertEqual((["\n"], ""), splitlines("\n", keep_ends=True))
-        self.assertEqual((["foo\n", "bar\n"], ""), splitlines("foo\nbar\n", keep_ends=True))
-        self.assertEqual((["foo\n"], "bar"), splitlines("foo\nbar", keep_ends=True))
-        self.assertEqual((["foo\n", "\n", "bar\n"], ""), splitlines("foo\n\nbar\n", keep_ends=True))
-
     def test_split_lines(self):
         def expand(lines, remainder): return list(lines), remainder
 
