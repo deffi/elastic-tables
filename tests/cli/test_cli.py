330c73401f8698fd4e6cad045e917e58d976dc7f
Martin Herrmann <martin@famherrmann.de>
2022-11-04 18:35:45 +0100

    Use path to imported module

--- tests/cli/test_cli.py
+++ tests/cli/test_cli.py
@@ -3,6 +3,7 @@ import subprocess
 import sys
 import unittest
 
+from elastic_tabs.cli import cli
 from data import test_cases
 
 
@@ -10,16 +11,14 @@ class CliTest(unittest.TestCase):
     """We use text for the output so we get a better diff in the IDE.
     """
 
-    script_path = Path(__file__).parent.parent.parent / "src" / "elastic_tabs" / "cli" / "cli.py"
-
     def test_file(self):
         for prefix, input_path, expected_text in test_cases():
             with self.subTest(prefix):
-                output = subprocess.check_output([sys.executable, self.script_path, input_path], text=True)
+                output = subprocess.check_output([sys.executable, cli.__file__, input_path], text=True)
                 self.assertEqual(expected_text, output)
 
     def test_stdin(self):
-        args = [sys.executable, self.script_path]
+        args = [sys.executable, cli.__file__]
         for prefix, input_path, expected_text in test_cases():
             with self.subTest(prefix):
                 # Pass the input as text because the text parameter applies to both
