a187fa24fc232f82d264251744f9d275d8822208
Martin Herrmann <martin@famherrmann.de>
2022-11-05 13:43:57 +0100

    Enumerate test cases explicitly
    We may need different options for some tests

--- tests/cli/test_cli.py
+++ tests/cli/test_cli.py
@@ -1,30 +1,37 @@
+from pathlib import Path
 import subprocess
 import sys
 import unittest
 
 from elastic_tabs.cli import cli
-from data import test_cases
+import data as test_data
 
 
 class CliTest(unittest.TestCase):
-    """We use text for the output so we get a better diff in the IDE.
-    """
+    def _test_file(self, input_path: Path, expected_path: Path):
+        output = subprocess.check_output([sys.executable, cli.__file__, input_path])
+        expected = expected_path.read_bytes()
+        self.assertEqual(expected, output)
 
-    def test_file(self):
-        for prefix, input_path, expected_path in test_cases():
-            with self.subTest(prefix):
-                output = subprocess.check_output([sys.executable, cli.__file__, input_path])
-                self.assertEqual(expected_path.read_bytes(), output)
+    def _test_stdin(self, input_path: Path, expected_path: Path):
+        input_ = input_path.read_bytes()
+        output = subprocess.check_output([sys.executable, cli.__file__], input=input_)
+        expected = expected_path.read_bytes()
+        self.assertEqual(expected, output)
 
-    def test_stdin(self):
-        for prefix, input_path, expected_path in test_cases():
-            with self.subTest(prefix):
-                input_ = input_path.read_bytes()
-                output = subprocess.check_output([sys.executable, cli.__file__], input=input_)
-                print(repr(input_))
-                print(repr(output))
-                print()
-                self.assertEqual(expected_path.read_bytes(), output)
+    def _test(self, prefix: str):
+        with self.subTest(prefix=prefix):
+            input_path, expected_path = test_data.test_case(prefix)
+
+            with self.subTest(method="file"):
+                self._test_file(input_path, expected_path)
+
+            with self.subTest(method="stdin"):
+                self._test_stdin(input_path, expected_path)
+
+    def test_line_break(self):
+        self._test("line-break_lf")
+        self._test("line-break_crlf")
 
     # TODO test that after the first chunk, the first table is output before the
     # second chunk is complete
--- tests/data/__init__.py
+++ tests/data/__init__.py
@@ -2,8 +2,20 @@ from pathlib import Path
 from typing import Iterator, Tuple
 
 
+_root = Path(__file__).parent
+
+
+def test_case(prefix: str) -> Tuple[Path, Path]:
+    """Returns input_path, expected_path"""
+
+    input_path = _root / f"{prefix}_in.txt"
+    expected_path = _root / f"{prefix}_expected.txt"
+
+    return input_path, expected_path
+
+
 def test_cases() -> Iterator[Tuple[str, Path, Path]]:
-    """Yields prefix, input_path, output_path"""
+    """Yields prefix, input_path, expected_path"""
 
     for input_path in Path(__file__).parent.glob("*_in.txt"):
         prefix = input_path.stem.removesuffix("_in")
