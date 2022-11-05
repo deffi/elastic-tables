e5350c42fa7c2f2eeca18a5ba6c1a471ef0f85f3
Martin Herrmann <martin@famherrmann.de>
2022-11-05 11:58:51 +0100

    Don't use text mode
    Catches an error where cli normalizes newlines to the current platform

--- tests/cli/test_cli.py
+++ tests/cli/test_cli.py
@@ -13,20 +13,18 @@ class CliTest(unittest.TestCase):
     def test_file(self):
         for prefix, input_path, expected_path in test_cases():
             with self.subTest(prefix):
-                output = subprocess.check_output([sys.executable, cli.__file__, input_path], text=True)
-                self.assertEqual(expected_path.read_text(), output)
+                output = subprocess.check_output([sys.executable, cli.__file__, input_path])
+                self.assertEqual(expected_path.read_bytes(), output)
 
     def test_stdin(self):
         for prefix, input_path, expected_path in test_cases():
             with self.subTest(prefix):
-                # Pass the input as text because the text parameter applies to both
-                # input and output
-                input_ = input_path.read_text()
-                output = subprocess.check_output([sys.executable, cli.__file__], input=input_, text=True)
+                input_ = input_path.read_bytes()
+                output = subprocess.check_output([sys.executable, cli.__file__], input=input_)
                 print(repr(input_))
                 print(repr(output))
                 print()
-                self.assertEqual(expected_path.read_text(), output)
+                self.assertEqual(expected_path.read_bytes(), output)
 
     # TODO test that after the first chunk, the first table is output before the
     # second chunk is complete
