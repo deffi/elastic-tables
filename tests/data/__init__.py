3ac2a38fedf9e14c39dcf472d812afdb0602066d
Martin Herrmann <martin@famherrmann.de>
2022-11-05 13:50:38 +0100

    Remove obsolete function
    It required Python 3.9

--- tests/data/__init__.py
+++ tests/data/__init__.py
@@ -12,13 +12,3 @@ def test_case(prefix: str) -> Tuple[Path, Path]:
     expected_path = _root / f"{prefix}_expected.txt"
 
     return input_path, expected_path
-
-
-def test_cases() -> Iterator[Tuple[str, Path, Path]]:
-    """Yields prefix, input_path, expected_path"""
-
-    for input_path in Path(__file__).parent.glob("*_in.txt"):
-        prefix = input_path.stem.removesuffix("_in")
-        expected_path = input_path.with_stem(f"{prefix}_expected")
-
-        yield prefix, input_path, expected_path
