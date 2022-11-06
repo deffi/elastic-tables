bbb5c8b917247b367b9ffa63bf5f8dbd0e8255c6
Martin Herrmann <martin@famherrmann.de>
2022-11-06 17:43:08 +0100

    Add numeric alignment

--- src/elastic_tables/rendering/renderer.py
+++ src/elastic_tables/rendering/renderer.py
@@ -1,22 +1,34 @@
-from typing import Sequence, Iterator
+import re
+from typing import Sequence, Iterator, Callable
 
 from elastic_tables.model import Table, Row
+
+numeric_pattern = re.compile(r'\s*[+-]?\d+\s*')
 
 
 class Renderer:
     def __init__(self):
-        ...
+        self.align_numeric = False
 
-    def render_cell(self, text: str, width: int) -> str:
+    # TODO be more specific than Callable
+    def render_cell(self, text: str, width: int, alignment: Callable) -> str:
         if len(text) > width:
             raise ValueError("Text too long")
 
-        return text.ljust(width)
+        return alignment(text, width)
 
-    def render_row(self, row: Row, widths: Sequence[int]) -> str:
-        rendered_cells = (self.render_cell(cell, width) for cell, width in zip (row.cells, widths))
+    def render_row(self, row: Row, widths: Sequence[int], alignments: Sequence[Callable]) -> str:
+        cell_width_alignment = zip (row.cells, widths, alignments)
+        rendered_cells = (self.render_cell(cell, width, alignment) for cell, width, alignment in cell_width_alignment)
         return "".join(rendered_cells) + row.line_terminator
 
     def render(self, table: Table) -> Iterator[str]:
         columns_widths = table.column_widths()
-        return (self.render_row(row, columns_widths) for row in table.rows)
+
+        if self.align_numeric:
+            column_is_numeric = table.columns_match(numeric_pattern)
+            default_columns_alignment = [str.rjust if numeric else str.ljust for numeric in column_is_numeric]
+        else:
+            default_columns_alignment = [str.ljust] * len(columns_widths)
+
+        return (self.render_row(row, columns_widths, default_columns_alignment) for row in table.rows)
--- tests/rendering/test_renderer.py
+++ tests/rendering/test_renderer.py
@@ -7,21 +7,25 @@ from elastic_tables.rendering import Renderer
 class RendererTest(unittest.TestCase):
     def test_render_cell(self):
         # Empty string
-        self.assertEqual("", Renderer().render_cell("", 0))
-        self.assertEqual("    ", Renderer().render_cell("", 4))
+        self.assertEqual("", Renderer().render_cell("", 0, str.ljust))
+        self.assertEqual("    ", Renderer().render_cell("", 4, str.ljust))
 
         # Pad
-        self.assertEqual("foo ", Renderer().render_cell("foo", 4))
+        self.assertEqual("foo ", Renderer().render_cell("foo", 4, str.ljust))
 
         # Fits
-        self.assertEqual("foo", Renderer().render_cell("foo", 3))
+        self.assertEqual("foo", Renderer().render_cell("foo", 3, str.ljust))
 
         # Too long
         with self.assertRaises(ValueError):
-            Renderer().render_cell("foobar", 4)
+            Renderer().render_cell("foobar", 4, str.ljust)
 
     def test_render_row(self):
-        self.assertEqual("a  foo  bc  \n", Renderer().render_row(Row(["a", "foo", "b", "c"], "\n"), [3, 5, 1, 3]))
+        l = str.ljust
+        r = str.rjust
+        self.assertEqual("a  foo  bc  \n", Renderer().render_row(Row(["a", "foo", "b", "c"], "\n"), [3, 5, 1, 3], [l, l, l, l]))
+        self.assertEqual("a    foobc  \n", Renderer().render_row(Row(["a", "foo", "b", "c"], "\n"), [3, 5, 1, 3], [l, r, l, l]))
+        self.assertEqual("  a  foob  c\n", Renderer().render_row(Row(["a", "foo", "b", "c"], "\n"), [3, 5, 1, 3], [r, r, r, r]))
 
     def test_render_table(self):
         # Empty table
@@ -45,6 +49,19 @@ class RendererTest(unittest.TestCase):
             list(Renderer().render(Table([
                 Row(["foo", "b"], "\n"),
                 Row(["f"], "\n")]))))
+
+    def test_align_numeric(self):
+        table = Table([
+            Row(["a"  , "+1" , "222"], "\n"),
+            Row(["bb" , "333", "44" ], "\n"),
+            Row(["ccc", "55" , "d"  ], "\n"),
+        ])
+
+        renderer = Renderer()
+        self.assertEqual(["a  +1 222\n", "bb 33344 \n", "ccc55 d  \n"], list(renderer.render(table)))
+        renderer.align_numeric = True
+        self.assertEqual(["a   +1222\n", "bb 33344 \n", "ccc 55d  \n"], list(renderer.render(table)))
+
 
 
 if __name__ == '__main__':
