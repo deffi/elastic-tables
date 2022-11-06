837565616db281d61e74d33359a83e91d337114f
Martin Herrmann <martin@famherrmann.de>
2022-11-06 15:50:36 +0100

    More flexible unit test

--- tests/test_io.py
+++ tests/test_io.py
@@ -2,20 +2,22 @@ import sys
 import unittest
 import subprocess
 
-import elastic_tables.io
-
 
 class IoTest(unittest.TestCase):
-    def test_stdout(self):
-        output = subprocess.check_output([sys.executable, __file__], text=True)
+    @staticmethod
+    def do_stdout_with_install():
+        import elastic_tables.io
+        elastic_tables.io.install()
+        print("foo\tb")
+        print("f\tbar")
+
+    def test_stdout_with_install(self):
+        output = subprocess.check_output([sys.executable, __file__, "do_stdout_with_install"], text=True)
         self.assertEqual("foob  \nf  bar\n", output)
 
 
 if __name__ == '__main__':
-    # unittest.main()
-
-    import elastic_tables.io
-
-    elastic_tables.io.install()
-    print("foo\tb")
-    print("f\tbar")
+    if len(sys.argv) == 1:
+        unittest.main()
+    else:
+        getattr(IoTest, sys.argv[1])(*sys.argv[2:])
