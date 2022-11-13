557d2d0ba5289785073fc3d9ab69dad7a7205362
Martin Herrmann <martin@famherrmann.de>
2022-11-13 12:45:11 +0100

    Get rid of function columns_match

--- src/elastic_tables/model/table.py
+++ src/elastic_tables/model/table.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from itertools import zip_longest
 import re
-from typing import Sequence, Iterator, Pattern
+from typing import Sequence, Iterator
 
 from elastic_tables.model import Row, Cell, Block, Column
 from elastic_tables.util.alignment import left, right
@@ -23,15 +23,12 @@ class Table:
         all_cells = zip_longest(*(row.cells for row in self.rows), fillvalue=Cell(""))
         return (Column(list(column_cells)) for column_cells in all_cells)
 
-    def columns_match(self, pattern: Pattern) -> Sequence[bool]:
-        columns = self.columns()
-        return [column.matches(pattern) for column in columns]
-
     def render(self, align_numeric: bool = False) -> Iterator[str]:
-        column_widths = [column.width() for column in self.columns()]
+        columns = list(self.columns())
+        column_widths = [column.width() for column in columns]
 
         if align_numeric:
-            column_is_numeric = self.columns_match(numeric_pattern)
+            column_is_numeric = [column.matches(numeric_pattern) for column in columns]
             default_columns_alignment = [right if numeric else left for numeric in column_is_numeric]
         else:
             default_columns_alignment = [left] * len(column_widths)
