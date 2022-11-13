38971a923de8d39e8ae371a572d89d040e39dcce
Martin Herrmann <martin@famherrmann.de>
2022-11-13 11:57:39 +0100

    Cleanup

--- tests/model/test_row.py
+++ tests/model/test_row.py
@@ -10,9 +10,10 @@ class RowTest(unittest.TestCase):
     def test_render(self):
         l = str.ljust
         r = str.rjust
-        self.assertEqual("a  foo  bc  \n", Row([Cell("a"), Cell("foo"), Cell("b"), Cell("c")], "\n").render([3, 5, 1, 3], [l, l, l, l]))
-        self.assertEqual("a    foobc  \n", Row([Cell("a"), Cell("foo"), Cell("b"), Cell("c")], "\n").render([3, 5, 1, 3], [l, r, l, l]))
-        self.assertEqual("  a  foob  c\n", Row([Cell("a"), Cell("foo"), Cell("b"), Cell("c")], "\n").render([3, 5, 1, 3], [r, r, r, r]))
+        row = Row([Cell("a"), Cell("foo"), Cell("b"), Cell("c")], "\n")
+        self.assertEqual("a  foo  bc  \n", row.render([3, 5, 1, 3], [l, l, l, l]))
+        self.assertEqual("a    foobc  \n", row.render([3, 5, 1, 3], [l, r, l, l]))
+        self.assertEqual("  a  foob  c\n", row.render([3, 5, 1, 3], [r, r, r, r]))
 
 
 if __name__ == '__main__':
