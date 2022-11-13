b8b3664c417f85aa1c5858d78bec2bcfe3cbca18
Martin Herrmann <martin@famherrmann.de>
2022-11-13 11:50:15 +0100

    Simplify TableGenerator

--- src/elastic_tables/parsing/table_generator.py
+++ src/elastic_tables/parsing/table_generator.py
@@ -7,11 +7,6 @@ class TableGenerator:
     def __init__(self):
         pass
 
-    def _row_from_line(self, line: Line) -> Row:
-        # TODO defer to Row.from_line?
-        cells = [Cell(text) for text in line.content.split(self.separator)]
-        return Row(cells, line.terminator)
-
     def table_from_block(self, block: Block) -> Table:
-        rows = (self._row_from_line(line) for line in block.lines)
+        rows = (Row.from_line(line, self.separator) for line in block.lines)
         return Table(list(rows))
