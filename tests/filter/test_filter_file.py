55d52795d9f2fde3cf2ff1669bebfa0218dd8c59
Martin Herrmann <martin@famherrmann.de>
2022-12-11 11:01:06 +0100

    Add other tests

--- tests/filter/test_filter_file.py
+++ tests/filter/test_filter_file.py
@@ -9,19 +9,21 @@ import data as test_data
 
 
 class FilterTest(unittest.TestCase):
-    def _test(self, prefix: str, filter_args: dict = None, column_separator: Optional[str] = "|"):
+    def _test(self, prefix: str, filter_args: dict = None, suffix: Optional[str] = "", *,
+              column_separator: Optional[str] = "|"):
         if filter_args is None:
             filter_args = {}
 
         if column_separator is not None:
             filter_args["column_separator"] = column_separator
 
-        input_path, expected_path = test_data.test_case(prefix)
+        input_path, expected_path = test_data.test_case(prefix, suffix)
 
         input_ = input_path.read_text()
         expected = expected_path.read_text()
 
-        self.assertEqual(expected, "".join(Filter.filter(input_, **(filter_args or {}))))
+        with self.subTest(prefix=prefix, suffix=suffix, filter_args=filter_args):
+            self.assertEqual(expected, "".join(Filter.filter(input_, **filter_args)))
 
     def test_column_separator(self):
         self._test("column-separator_tab", column_separator=None)  # The filter defaults to tab
@@ -33,7 +35,15 @@ class FilterTest(unittest.TestCase):
         self._test("line-break_lf")
         self._test("line-break_crlf")
 
-    # TODO test all - see test_cli
+    def test_align_numeric(self):
+        self._test("align-numeric", {}, "no")
+        self._test("align-numeric", {"align_numeric": True}, "yes")
+        self._test("align-numeric", {"align_numeric": False}, "no")
+
+    def test_align_space(self):
+        self._test("align-space", {}, "no")
+        self._test("align-space", {"align_space": True}, "yes")
+        self._test("align-space", {"align_space": False}, "no")
 
 
 if __name__ == '__main__':
