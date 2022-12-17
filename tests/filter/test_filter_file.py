8f80caa8a04582db33d37715bcae0f3f5ebfed3c
Martin Herrmann <martin@famherrmann.de>
2022-12-17 20:25:34 +0100

    Use pytest parametrization instead of subtests

--- tests/filter/test_filter_file.py
+++ tests/filter/test_filter_file.py
@@ -1,4 +1,4 @@
-from typing import Optional
+import pytest
 
 from elastic_tables.filter import Filter
 
@@ -6,35 +6,34 @@ import data as test_data
 
 
 class TestFilter:
-    def _test(self, subtests, prefix: str, filter_args: dict = None, suffix: Optional[str] = "", *,
-              column_separator: Optional[str] = "|"):
-        if filter_args is None:
-            filter_args = {}
+    @pytest.mark.parametrize("prefix, filter_args, suffix", [
+        # Column separators
+        ["column-separator_tab" , {"column_separator": "\t"}, ""],  # Explicit tab
+        ["column-separator_pipe", {"column_separator": "|"},  ""],  # Explicit pipe
+        ["column-separator_tab" , {"column_separator": ...},  ""],  # The filter defaults to tab
+        ["column-separator_pipe", {},                         ""],  # This test defaults to pipe
 
-        if column_separator is not None:
-            filter_args["column_separator"] = column_separator
+        # Line breaks
+        ["line-break_lf"  , {}, ""],  # LF
+        ["line-break_crlf", {}, ""],  # CR/LF
+
+        # Numeric alignment
+        ["align-numeric", {"align_numeric": True }, "yes"],  # True
+        ["align-numeric", {"align_numeric": False}, "no" ],  # False
+        ["align-numeric", {}                      , "no" ],  # Filter default: False
+
+        # Space alignment
+        ["align-space", {"align_space": True }, "yes"],  # True
+        ["align-space", {"align_space": False}, "no" ],  # False
+        ["align-space", {}                    , "no" ],  # Filter default: False
+    ], ids=str)
+    def test_filter(self, prefix: str, filter_args: dict, suffix: str):
+        # If column_separator is not specified, use "|" (test default). If it is
+        # ..., remove (filter default).
+        if "column_separator" not in filter_args:
+            filter_args["column_separator"] = "|"
+        elif filter_args["column_separator"] is ...:
+            del filter_args["column_separator"]
 
         input_, expected = test_data.test_case_data(prefix, suffix)
-
-        with subtests.test(prefix=prefix, suffix=suffix, filter_args=filter_args):
-            assert "".join(Filter.filter(input_, **filter_args)) == expected
-
-    def test_column_separator(self, subtests):
-        self._test(subtests, "column-separator_tab", column_separator="\t")  # Explicit tab
-        self._test(subtests, "column-separator_pipe", column_separator="|")  # Explicit pipe
-        self._test(subtests, "column-separator_tab", column_separator=None)  # The filter defaults to tab
-        self._test(subtests, "column-separator_pipe")  # This test defaults to pipe
-
-    def test_line_break(self, subtests):
-        self._test(subtests, "line-break_lf")  # LF
-        self._test(subtests, "line-break_crlf")  # CR/LF
-
-    def test_align_numeric(self, subtests):
-        self._test(subtests, "align-numeric", {"align_numeric": True}, "yes")  # Explicit true
-        self._test(subtests, "align-numeric", {"align_numeric": False}, "no")  # Explicit false
-        self._test(subtests, "align-numeric", {}, "no")  # The filter defaults to false
-
-    def test_align_space(self, subtests):
-        self._test(subtests, "align-space", {"align_space": True}, "yes")  # Explicit true
-        self._test(subtests, "align-space", {"align_space": False}, "no")  # Explicit false
-        self._test(subtests, "align-space", {}, "no")  # The filter defaults to false
+        assert "".join(Filter.filter(input_, **filter_args)) == expected
