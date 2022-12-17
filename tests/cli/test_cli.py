5ef31c894d55fae24d406d243c7ce5b694a18e24
Martin Herrmann <martin@famherrmann.de>
2022-12-17 20:46:43 +0100

    Use pytest parametrization instead of subtests
    Input methods (stdin/file) still don't use parametrization

--- tests/cli/test_cli.py
+++ tests/cli/test_cli.py
@@ -4,6 +4,8 @@ import os
 import sys
 from typing import Optional, List
 
+import pytest
+
 from elastic_tables.cli import cli
 import data as test_data
 
@@ -20,45 +22,42 @@ class TestCli:
         expected = expected_path.read_bytes()
         assert output == expected
 
-    def _test(self, subtests, prefix: str, args: Optional[List[str]] = None, suffix: Optional[str] = "", *,
-              column_separator: Optional[str] = "|"):
-        if args is None:
-            args = []
-
-        if column_separator is not None:
-            args = ["--column-separator", column_separator] + args
-
-        with subtests.test(prefix=prefix, suffix=suffix, args=args):
-            input_path, expected_path = test_data.test_case(prefix, suffix)
-
-            with subtests.test(method="file"):
-                self._test_file(input_path, expected_path, args)
-
-            with subtests.test(method="stdin"):
-                self._test_stdin(input_path, expected_path, args)
-
-    def test_column_separator(self, subtests):
-        self._test(subtests, "column-separator_tab", column_separator="\t")  # Explicit tab
-        self._test(subtests, "column-separator_pipe", column_separator="|")  # Explicit pipe
-        self._test(subtests, "column-separator_tab", column_separator=None)  # The CLI defaults to tab
-        self._test(subtests, "column-separator_pipe")  # This test defaults to pipe
-
-    def test_line_break(self, subtests):
-        self._test(subtests, "line-break_lf")  # LF
-        self._test(subtests, "line-break_crlf")  # CR/LF
-
-    def test_align_numeric(self, subtests):
-        self._test(subtests, "align-numeric", ["--align-numeric"], "yes")  # Explicit true
-        self._test(subtests, "align-numeric", ["--no-align-numeric"], "no")  # Explicit false
-        self._test(subtests, "align-numeric", [], "no")  # The CLI defaults to false
-
-    def test_align_space(self, subtests):
-        self._test(subtests, "align-space", ["--align-space"], "yes")  # Explicit true
-        self._test(subtests, "align-space", ["--no-align-space"], "no")  # Explicit false
-        self._test(subtests, "align-space", [], "no")  # The CLI defaults to false
-
     # TODO test that after the first chunk, the first table is output before the
     # second chunk is complete
+
+    @pytest.mark.parametrize("prefix, args, suffix", [
+        # Column separators
+        ["column-separator_tab" , ["--column-separator", "\t"], ""],  # Explicit tab
+        ["column-separator_pipe", ["--column-separator", "|" ], ""],  # Explicit pipe
+        ["column-separator_tab" , ["--column-separator", ... ], ""],  # The CLI defaults to tab
+        ["column-separator_pipe", [],                           ""],  # This test defaults to pipe
+
+        # Line breaks
+        ["line-break_lf"  , [], ""],  # LF
+        ["line-break_crlf", [], ""],  # CR/LF
+
+        # Numeric alignment
+        ["align-numeric", ["--align-numeric"   ], "yes"],  # True
+        ["align-numeric", ["--no-align-numeric"], "no" ],  # False
+        ["align-numeric", []                    , "no" ],  # CLI default: False
+
+        # Space alignment
+        ["align-space", ["--align-space"   ], "yes"],  # True
+        ["align-space", ["--no-align-space"], "no" ],  # False
+        ["align-space", []                  , "no" ],  # CLI default: False
+    ], ids=str)
+    def test_cli(self, prefix: str, args: List[str], suffix: str):
+        # If column_separator is not specified, use "|" (test default). If it is
+        # ..., remove (filter default).
+        if "--column-separator" not in args:
+            args.extend(["--column-separator", "|"])
+        elif args[args.index("--column-separator")+1] is ...:
+            args[args.index("--column-separator"):args.index("--column-separator")+2] = []
+
+        input_path, expected_path = test_data.test_case(prefix, suffix)
+
+        self._test_file(input_path, expected_path, args)
+        self._test_stdin(input_path, expected_path, args)
 
 
 if os.environ.get("SKIP_CLI_TEST") == "1":
